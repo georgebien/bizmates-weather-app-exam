bf259187972b7717563c8bb188e30ac786682759
George Bien A. Almenanza <gbienalmenanza@gmail.com>
2025-05-25 01:08:40 +0800

    [FRONTEND]: Initial setup

---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <weather-app></weather-app>
+  </div>
+</template>
+
+<script>
+import weatherApp from './views/weatherApp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    weatherApp
+  }
+}
+</script>
+
+<style>
+@import "./assets/css/main.css";
+#app {
+  min-height: 100vh;
+  height: 100%;
+  background-image: url('./assets/bg1.jpg');
+  background-size: cover;
+  color: rgb(36, 36, 36) !important;
+}
+</style>
